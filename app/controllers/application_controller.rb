a1352f6ad5429e1ead3cb79cb2bcb07dc8fafaca
towelthief <b.g188s@gmail.com>
2018-10-29 13:05:22 +0900

    enum roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,14 @@ class ApplicationController < ActionController::Base
       redirect_to login_path
     end
   end
+
+private
+
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   has_many :todo_lists, dependent: :destroy
   has_many :todo_items, dependent: :destroy
+
+  enum role: [:user, :admin]
+
+  after_initialize do
+  if self.new_record?
+    self.role ||= :user
+  end
+end
 end
